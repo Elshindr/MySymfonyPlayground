62350327bdbcb527e9a4f691a2920fbe2900a451
Elshindr <headkeytics@gmail.com>
2021-08-23 19:17:27 +0200

    push etape 1-5

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * Description of AccueilController
+ *
+ * @author Ydrani
+ */
+class AccueilController extends AbstractController{
+    /**
+     * 
+     * @Route("/", name="accueil")
+     * @return Response
+     */
+   public function index():Response{
+       return $this->render("pages/accueil.html.twig");
+   }
+}
