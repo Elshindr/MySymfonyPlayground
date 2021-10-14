6b690b8565afa26f384b09895c2692a7d83521f0
Elshindr <headkeytics@gmail.com>
2021-10-14 15:51:27 +0200

    phase 21: securite Front

--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -8,6 +8,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -24,7 +25,12 @@ class VisiteType extends AbstractType
             ->add('ville')
             ->add('pays')
             ->add('datecreation', null, ['label' => 'date de creation'])
-            ->add('note')
+            ->add('note', IntegerType::class,[
+                'attr'=> [
+                    'min'=> 0,
+                    'max'=>20
+                ]
+            ])
             ->add('avis')
             ->add('tempmin', null, ['label' => 'T° min'])
             ->add('tempmax', null, ['label' => 'T° max'])
